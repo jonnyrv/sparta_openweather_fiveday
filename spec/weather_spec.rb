3c9a7c3feab3bd6e9f4b8df5fac68f06f672f846
Tech-A04 <tech-a04@MacBook-Air-7.home>
2018-04-13 08:07:11 +0100

    Added tests

---
+++ spec/weather_spec.rb
@@ -0,0 +1,79 @@
+require 'spec_helper'
+
+describe Weather do
+
+    context 'requesting information on a single city weather works correctly' do
+        
+        before(:all) do
+            # @weather = Weather.new
+            # @response = @weather.get_single_weather('London') #input a city
+        end
+
+        it "should respond with a status message of 200" do
+            pending
+        end
+
+        it "should have a city hash" do
+            pending
+        end
+
+        it "should return a city string" do
+            pending
+        end
+
+        it "should return a country string" do
+            pending
+        end
+
+        it "should have a list array" do
+            pending
+        end
+
+        it "should return an temp_min value as float" do
+            pending
+        end
+
+        it "should return an temp_max value as float" do
+            pending
+        end
+
+        it "should return an  humidity value as integer" do
+            pending
+        end
+
+        it "should have a weather array" do
+            pending
+        end
+
+        it "should have a main inside weather which returns a string" do
+            pending
+        end
+
+        it "should have a description inside weather which returns a string" do
+            pending
+        end
+
+        it "should return a clouds value as integer" do
+            pending
+        end
+
+        it "should return a wind as a hash" do
+            pending
+        end
+
+        it "should return speed inside wind as a float value" do
+            pending
+        end
+
+        it "should return dt_txt as a string" do
+            pending
+        end
+
+        
+
+
+
+    end
+
+
+end
